cdfc7272b3f1bb7d442df69fc79160cde947b773
PETISOLUCIONES <ingenieria@peti.com.co>
2021-09-09 08:53:53 -0500

    ajuste

---
+++ ajuste_unidades_devolucion/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Corrección en unidades de devolución",
+
+    'summary': """
+        Evita el retorno de productos en las SO al realizar un ajuste.""",
+
+    'description': """
+        Las facturas rectificativas no descuentan la cantidad de productos en 
+        las ordenes de venta cuando el precio de retorno es diferente al 
+        precio de venta.
+    """,
+
+    'author': "PETI Soluciones Productivas",
+    'website': "https://peti.com.co",
+
+    'category': 'Uncategorized',
+    'version': '13.1',
+
+    'depends': [
+        'base',
+        'sale',
+        'account',
+    ],
+
+    'data': [
+    ],
+}
